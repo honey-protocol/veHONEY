5462a4a5b7f990f1d9287136c9aa8a00e151ab1a
Freezy Gem <freezy.gem0918@outlook.com>
2022-04-27 18:23:09 +0000

    feat: initlocker v2

--- programs/ve_honey/src/context/init_locker.rs
+++ programs/ve_honey/src/context/init_locker.rs
@@ -2,6 +2,7 @@ use crate::constants::*;
 use crate::state::*;
 use anchor_lang::prelude::*;
 use anchor_spl::token::Mint;
+use govern::Governor;
 use vipers::*;
 
 #[derive(Accounts)]
@@ -65,4 +66,69 @@ pub struct InitLockerEvent {
     pub admin: Pubkey,
     /// [LockerParams].
     pub params: LockerParams,
+}
+
+#[derive(Accounts)]
+pub struct InitLockerV2<'info> {
+    /// Payer of initialization.
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    /// Base.
+    pub base: Signer<'info>,
+    /// [Locker].
+    #[account(
+        init,
+        seeds = [LOCKER_SEED.as_bytes(), base.key().as_ref()],
+        bump,
+        space = 8 + LockerV2::LEN,
+        payer = payer
+    )]
+    pub locker: Box<Account<'info, LockerV2>>,
+    /// Mint of the token that can be used to join the [Locker].
+    pub token_mint: Box<Account<'info, Mint>>,
+    /// [Governor] associated with the [Locker].
+    pub governor: Box<Account<'info, Governor>>,
+
+    /// System program.
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> InitLockerV2<'info> {
+    pub fn process(&mut self, bump: u8, params: LockerParamsV2) -> Result<()> {
+        let locker = &mut self.locker;
+        locker.token_mint = self.token_mint.key();
+        locker.governor = self.governor.key();
+        locker.base = self.base.key();
+        locker.bump = bump;
+        locker.params = params;
+
+        emit!(InitLockerV2Event {
+            locker: locker.key(),
+            token_mint: locker.token_mint,
+            governor: locker.governor,
+            params,
+        });
+
+        Ok(())
+    }
+}
+
+impl<'info> Validate<'info> for InitLockerV2<'info> {
+    fn validate(&self) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[event]
+/// Event called in [ve_honey::init_locker].
+pub struct InitLockerV2Event {
+    /// The [Locker] being created.
+    #[index]
+    pub locker: Pubkey,
+    /// Mint of the token that can be used to join the [Locker].
+    pub token_mint: Pubkey,
+    /// The governor of the [Locker].
+    pub governor: Pubkey,
+    /// [LockerParams].
+    pub params: LockerParamsV2,
 }
